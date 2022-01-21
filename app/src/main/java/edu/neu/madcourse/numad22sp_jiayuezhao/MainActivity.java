318c62a565369fa3949dcf531f2202afdce2c629
lunazzao <zhao.jiayu@northeastern.edu>
2022-01-20 20:47:48 -0500

    Mobile Dev Project initial commit, 01/20/22

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_jiayuezhao/MainActivity.java
@@ -0,0 +1,26 @@
+package edu.neu.madcourse.numad22sp_jiayuezhao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toastMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void displayToastMsg(View v) {
+        toastMsg("Jiayue Zhao, zhao.jiayu@northeastern.edu");
+    }
+
+}

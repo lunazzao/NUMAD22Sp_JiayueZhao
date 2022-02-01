d44354896edf1012535799744d13c6b97d843191
lunazzao <zhao.jiayu@northeastern.edu>
2022-02-01 18:20:55 -0500

    Update with second week's material - button leading to another window

--- app/src/main/java/edu/neu/madcourse/numad22sp_jiayuezhao/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_jiayuezhao/MainActivity.java
@@ -2,16 +2,28 @@ package edu.neu.madcourse.numad22sp_jiayuezhao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
     }
 
     public void toastMsg(String msg) {
@@ -21,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void displayToastMsg(View v) {
         toastMsg("Jiayue Zhao, zhao.jiayu@northeastern.edu");
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 
 }

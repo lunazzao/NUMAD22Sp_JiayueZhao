7e438896f5fa55b7d37dff45d8a22eff9f7fbe8a
lunazzao <zhao.jiayu@northeastern.edu>
2022-02-04 15:38:18 -0500

    Week3 - Constraints, 6 buttons

--- app/src/main/java/edu/neu/madcourse/numad22sp_jiayuezhao/Activity2.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_jiayuezhao/Activity2.java
@@ -1,14 +1,72 @@
 package edu.neu.madcourse.numad22sp_jiayuezhao;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        /*
+        ConstraintLayout myCL = new ConstraintLayout(this);
+
+        myCL.addView();
+        myCL.addView(button);
+
+        setContentView(myCL);
+
+
+        ConstraintSet csForTV = new ConstraintSet();
+
+        csForTV.constrainHeight(myTV.getId(), ConstraintSet.WRAP_CONTENT);
+        csForTV.constrainWidth(myTV.getId(), ConstraintSet.WRAP_CONTENT);
+
+        csForTV.connect(myTV.getId(),ConstraintSet.START,ConstraintSet.PARENT_ID,ConstraintSet.START,0);
+        csForTV.connect(myTV.getId(),ConstraintSet.END,myButton.getId(),ConstraintSet.START,0);
+        csForTV.connect(myTV.getId(),ConstraintSet.TOP,ConstraintSet.PARENT_ID,ConstraintSet.TOP,0);
+
+        csForTV.applyTo(myCL);
+
+        ConstraintSet csForButton = new ConstraintSet();
+        csForButton.constrainHeight(myButton.getId(), ConstraintSet.WRAP_CONTENT);
+        csForButton.constrainWidth(myButton.getId(), ConstraintSet.WRAP_CONTENT);
+        csForButton.connect(myButton.getId(), ConstraintSet.START, myTV.getId(), ConstraintSet.END, 0);
+    */
+
         setContentView(R.layout.activity_2);
     }
+
+    @SuppressLint({"NonConstantResourceId", "SetTextI18n"})
+    public void onClick(View view){
+        TextView tv = (TextView) findViewById(R.id.textViewClicky);
+        switch (view.getId()){
+            case R.id.button:
+                tv.setText("Pressed: A");
+                break;
+            case R.id.button2:
+                tv.setText("Pressed: B");
+                break;
+            case R.id.button3:
+                tv.setText("Pressed: C");
+                break;
+            case R.id.button4:
+                tv.setText("Pressed: D");
+                break;
+            case R.id.button5:
+                tv.setText("Pressed: E");
+                break;
+            case R.id.button6:
+                tv.setText("Pressed: F");
+                break;
+        }
+    }
+
 }
--- app/src/main/java/edu/neu/madcourse/numad22sp_jiayuezhao/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_jiayuezhao/MainActivity.java
@@ -9,22 +9,56 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private Button clickybutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
+        //setContentView(R.layout.activity_main);
+        /*TextView myTV = new TextView(this);
+        myTV.setText("Message New");
+        myTV.setBackgroundColor(Color.CYAN);
+        myTV.setId(R.id.myTextView);
+        */
 
+
+        Button myButton = (Button) findViewById(R.id.clickyButton);
+        myButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                openActivity2();
+            public void onClick(View v) { openActivity2();
             }
         });
+
+        /*
+        ConstraintLayout myCL = new ConstraintLayout(this);
+        myCL.setBackgroundColor(Color.YELLOW);
+
+        myCL.addView(myTV);
+        myCL.addView(myButton);
+
+        setContentView(myCL);
+
+
+        ConstraintSet csForTV = new ConstraintSet();
+
+        csForTV.constrainHeight(myTV.getId(), ConstraintSet.WRAP_CONTENT);
+        csForTV.constrainWidth(myTV.getId(), ConstraintSet.WRAP_CONTENT);
+
+        csForTV.connect(myTV.getId(),ConstraintSet.START,ConstraintSet.PARENT_ID,ConstraintSet.START,0);
+        csForTV.connect(myTV.getId(),ConstraintSet.END,myButton.getId(),ConstraintSet.START,0);
+        csForTV.connect(myTV.getId(),ConstraintSet.TOP,ConstraintSet.PARENT_ID,ConstraintSet.TOP,0);
+
+        csForTV.applyTo(myCL);
+
+        ConstraintSet csForButton = new ConstraintSet();
+        csForButton.constrainHeight(myButton.getId(), ConstraintSet.WRAP_CONTENT);
+        csForButton.constrainWidth(myButton.getId(), ConstraintSet.WRAP_CONTENT);
+        csForButton.connect(myButton.getId(), ConstraintSet.START, myTV.getId(), ConstraintSet.END, 0);
+    */
     }
+
 
     public void toastMsg(String msg) {
         Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
@@ -34,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
     public void displayToastMsg(View v) {
         toastMsg("Jiayue Zhao, zhao.jiayu@northeastern.edu");
     }
+
 
     public void openActivity2(){
         Intent intent = new Intent(this, Activity2.class);

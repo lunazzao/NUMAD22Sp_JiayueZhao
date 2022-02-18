4c03fdaab32ef70f43643eabcce7448422825397
lunazzao <zhao.jiayu@northeastern.edu>
2022-02-18 18:30:54 -0500

    A5 initial commit - nothing works, just for check

--- app/src/main/java/edu/neu/madcourse/numad22sp_jiayuezhao/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_jiayuezhao/MainActivity.java
@@ -9,15 +9,11 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button clickbutton;
-    private Button linkbutton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     public void toastMsg(String msg) {
         Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
@@ -28,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         toastMsg("Jiayue Zhao, zhao.jiayu@northeastern.edu");
     }
 
-
     public void openActivity2(View v){
         Intent intent = new Intent(this, Activity2.class);
         startActivity(intent);
@@ -38,8 +33,13 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Activity3.class);
         startActivity(intent);
     }
+
     public void openActivity4(View v){
         Intent intent = new Intent(this, Activity4.class);
+        startActivity(intent);
+    }
+    public void openActivity5(View v){
+        Intent intent = new Intent(this, Activity5.class);
         startActivity(intent);
     }
 

77a3cf6032ec010c0945e07b7ae4f08a407e0348
lunazzao <zhao.jiayu@northeastern.edu>
2022-03-15 16:11:22 -0400

    A8 - Trying to fix the main page about a6

--- app/src/main/java/edu/neu/madcourse/numad22sp_jiayuezhao/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_jiayuezhao/MainActivity.java
@@ -46,5 +46,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Activity5.class);
         startActivity(intent);
     }
+    public void openActivity6(View v){
+        Intent intent = new Intent(this, Activity6.class);
+        startActivity(intent);
+    }
 
 }
